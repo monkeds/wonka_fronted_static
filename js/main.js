c6db837f35cc04bf05c247d16019649ef80d353a
monkar <medient@hotmail.com>
2016-05-08 18:14:57 -0500

    se agrego secciones experiencia y conocimientos

--- js/main.js
+++ js/main.js
@@ -14,16 +14,31 @@ function loadComponents(){
 	$('.ui.checkbox').checkbox();
 	
 	///
-	$('.dropdown').dropdown();
+	$('.link.dropdown').dropdown({
+		action: 'hide'
+		});
 
 	$('.accordion').accordion();
 	
 	$('.progress').progress();
 
+	$('.modal').modal();
 
+	
 	//menus laterales
 	$('#wrapper>#content>#left_content>nav .item').tab({
 		context:$("#wrapper>#content>#center_content")
 	});
 
+
+	$('.tabContainer>.menu>.item:not(.disabled)')
+	.tab({
+		context: $('.tabContainer')
+	});
+
+
+	$('#btnContact').click(function(){
+		$('#contactModal').modal("show");
+	});
 }
+
